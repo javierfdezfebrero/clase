e00862d50c0d958aac39c868abdbdc3e20622653
daw2FPD - Fernández Febrero <a15javierff@a32eql11.sanclemente.local>
2022-02-01 10:24:26 +0100

    Subida  01/02/2022 10:24

--- Exercicios/dwcs/ud4-php_sessions/folla4.5/lingua.php
+++ Exercicios/dwcs/ud4-php_sessions/folla4.5/lingua.php
@@ -6,4 +6,20 @@
 if (isset($_GET['lingua'])) {
 
     $lingua=$_GET['lingua'];
+
+    switch ($lingua) {
+        case 'es':
+            echo "<h1></h1>";
+        break;
+        case 'gl':
+            echo "<h1></h1>";
+            break;
+        case 'pt':
+            echo "<h1></h1>";
+            break;
+    
+        default:
+            echo "<h1></h1>";
+            break;
+    }
 }
